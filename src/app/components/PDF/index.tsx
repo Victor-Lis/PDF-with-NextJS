f190470727d39bbb2650957d6ae99122e02936e9
Victor-Lis <victorlisbronzo1@gmail.com>
2024-03-30 16:01:41 -0300

    v1.01
    - Fazendo tela virar um pdf

---
+++ src/app/components/PDF/index.tsx
@@ -0,0 +1,45 @@
+"use client";
+
+import {
+  Document,
+  Page,
+  Text,
+  View,
+  StyleSheet,
+  PDFViewer,
+} from "@react-pdf/renderer";
+
+const styles = StyleSheet.create({
+  page: {
+    backgroundColor: "#fff",
+    color: "#202020",
+  },
+  section: {
+    margin: 10,
+    padding: 10,
+  },
+  viewer: {
+    width: window.innerWidth, 
+    height: window.innerHeight,
+  },
+});
+
+function BasicDocument() {
+  return (
+    <PDFViewer style={styles.viewer}>
+      {/* Start of the document*/}
+      <Document>
+        {/*render a single page*/}
+        <Page size="A4" style={styles.page}>
+          <View style={styles.section}>
+            <Text>Hello</Text>
+          </View>
+          <View style={styles.section}>
+            <Text>World</Text>
+          </View>
+        </Page>
+      </Document>
+    </PDFViewer>
+  );
+}
+export default BasicDocument;
